b2b3c2e3f6f94fbc271d0e0bd80fc06e51955b2d
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-09-28 09:06:17 -0700

    making list items dynamic

--- app/src/main/java/com/bakeaaro/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bakeaaro/criminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,11 +44,13 @@ public class CrimeListFragment extends Fragment {
 
         private TextView mTitleView;
         private TextView mDateTextView;
+        private ImageView mSolvedImageView;
 
         public void bind(Crime crime) {
             mCrime = crime;
             mTitleView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
@@ -56,6 +59,7 @@ public class CrimeListFragment extends Fragment {
 
             mTitleView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
 
         }
 
