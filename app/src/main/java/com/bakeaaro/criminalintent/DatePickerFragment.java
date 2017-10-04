cacf5d54f5c5d4ba58ed4340c168ddc6bf1f927b
Aaronmb123 <bakeaaro@oregonstate.edu>
2017-10-03 22:20:14 -0700

    passing data to datePickerFragment

--- app/src/main/java/com/bakeaaro/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/bakeaaro/criminalintent/DatePickerFragment.java
@@ -6,11 +6,37 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class DatePickerFragment extends DialogFragment {
+
+    private static final String ARG_DATE = "date";
+
+    private DatePicker mDatePicker;
+
+    public static DatePickerFragment newInstance(Date date) {
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_date, null);
         return new AlertDialog.Builder(getActivity())
                 .setView(view)
@@ -18,5 +44,7 @@ public class DatePickerFragment extends DialogFragment {
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
     }
+
+
 
 }
